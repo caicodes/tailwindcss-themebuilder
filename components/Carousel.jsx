3d1436d3da828799b6a800b5e9f3bded557ad1b8
caicodes <caicodes1@gmail.com>
2022-09-30 17:56:23 -0600

    upgraded images to next/image

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -1,56 +1,59 @@
-import React from "react";
+import Image from "next/image";
 
 const Carousel = () => {
   return (
     <div>
       <div className="carousel carousel-center p-12 space-x-12 bg-neutral  w-screen">
         <div className="carousel-item">
-          <img src="https://placeimg.com/640/480/animals" className="rounded" />
+          <Image
+            src="https://placeimg.com/640/480/animals"
+            className="rounded"
+          />
         </div>
         <div className="carousel-item">
-          <img
+          <Image
             src="https://placeimg.com/640/480/people"
             className="rounded-2xl"
           />
         </div>
         <div className="carousel-item">
-          <img
+          <Image
             src="https://placeimg.com/640/480/arch"
             className="rounded-2xl"
           />
         </div>
         <div className="carousel-item">
-          <img
+          <Image
             src="https://placeimg.com/640/480/tech"
             className="rounded-2xl"
           />
         </div>
         <div className="carousel-item">
-          <img
+          <Image
             src="https://placeimg.com/640/480/people"
             className="rounded-2xl"
           />
         </div>
         <div className="carousel-item">
-          <img
+          <Image
             src="https://placeimg.com/640/480/arch"
             className="rounded-2xl"
           />
         </div>
         <div className="carousel-item">
-          <img
+          <Image
             src="https://placeimg.com/640/480/nature"
             className="rounded-2xl"
           />
         </div>
         <div className="carousel-item">
-          <img
+          <Image
             src="https://placeimg.com/640/480/animals"
             className="rounded-2xl"
           />
         </div>
         <div className="carousel-item">
-          <img
+          <Image
             src="https://placeimg.com/640/480/tech"
             className="rounded-2xl"
           />
@@ -59,7 +62,10 @@ const Carousel = () => {
 
       <div className="carousel w-full">
         <div id="slide1" className="carousel-item relative w-full">
-          <img src="https://placeimg.com/1100/400/people" className="w-full" />
+          <Image
+            src="https://placeimg.com/1100/400/people"
+            className="w-full"
+          />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide4" className="btn btn-circle">
               ❮
@@ -70,7 +76,10 @@ const Carousel = () => {
           </div>
         </div>
         <div id="slide2" className="carousel-item relative w-full">
-          <img src="https://placeimg.com/1100/400/animals" className="w-full" />
+          <Image
+            src="https://placeimg.com/1100/400/animals"
+            className="w-full"
+          />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide1" className="btn btn-circle">
               ❮
@@ -81,7 +90,7 @@ const Carousel = () => {
           </div>
         </div>
         <div id="slide3" className="carousel-item relative w-full">
-          <img src="https://placeimg.com/1100/400/arch" className="w-full" />
+          <Image src="https://placeimg.com/1100/400/arch" className="w-full" />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide2" className="btn btn-circle">
               ❮
@@ -92,7 +101,7 @@ const Carousel = () => {
           </div>
         </div>
         <div id="slide4" className="carousel-item relative w-full">
-          <img src="https://placeimg.com/1100/400/tech" className="w-full" />
+          <Image src="https://placeimg.com/1100/400/tech" className="w-full" />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide3" className="btn btn-circle">
               ❮
