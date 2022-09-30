ae685c26958c6ed6cf3f0d0d2293b309ea110c2e
caicodes <caicodes1@gmail.com>
2022-09-30 17:59:22 -0600

    upgraded images to next/image

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -8,54 +8,63 @@ const Carousel = () => {
           <Image
             src="https://placeimg.com/640/480/animals"
             className="rounded"
+            alt=""
           />
         </div>
         <div className="carousel-item">
           <Image
             src="https://placeimg.com/640/480/people"
             className="rounded-2xl"
+            alt=""
           />
         </div>
         <div className="carousel-item">
           <Image
             src="https://placeimg.com/640/480/arch"
             className="rounded-2xl"
+            alt=""
           />
         </div>
         <div className="carousel-item">
           <Image
             src="https://placeimg.com/640/480/tech"
             className="rounded-2xl"
+            alt=""
           />
         </div>
         <div className="carousel-item">
           <Image
             src="https://placeimg.com/640/480/people"
             className="rounded-2xl"
+            alt=""
           />
         </div>
         <div className="carousel-item">
           <Image
             src="https://placeimg.com/640/480/arch"
             className="rounded-2xl"
+            alt=""
           />
         </div>
         <div className="carousel-item">
           <Image
             src="https://placeimg.com/640/480/nature"
             className="rounded-2xl"
+            alt=""
           />
         </div>
         <div className="carousel-item">
           <Image
             src="https://placeimg.com/640/480/animals"
             className="rounded-2xl"
+            alt=""
           />
         </div>
         <div className="carousel-item">
           <Image
             src="https://placeimg.com/640/480/tech"
             className="rounded-2xl"
+            alt=""
           />
         </div>
       </div>
@@ -65,6 +74,7 @@ const Carousel = () => {
           <Image
             src="https://placeimg.com/1100/400/people"
             className="w-full"
+            alt=""
           />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide4" className="btn btn-circle">
@@ -79,6 +89,7 @@ const Carousel = () => {
           <Image
             src="https://placeimg.com/1100/400/animals"
             className="w-full"
+            alt=""
           />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide1" className="btn btn-circle">
@@ -90,7 +101,11 @@ const Carousel = () => {
           </div>
         </div>
         <div id="slide3" className="carousel-item relative w-full">
-          <Image src="https://placeimg.com/1100/400/arch" className="w-full" />
+          <Image
+            src="https://placeimg.com/1100/400/arch"
+            className="w-full"
+            alt=""
+          />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide2" className="btn btn-circle">
               ❮
@@ -101,7 +116,11 @@ const Carousel = () => {
           </div>
         </div>
         <div id="slide4" className="carousel-item relative w-full">
-          <Image src="https://placeimg.com/1100/400/tech" className="w-full" />
+          <Image
+            src="https://placeimg.com/1100/400/tech"
+            className="w-full"
+            alt=""
+          />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide3" className="btn btn-circle">
               ❮
